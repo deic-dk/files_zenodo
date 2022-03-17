52bbe3227c94793c1c7b6c6c089e04afa2d5509e
Frederik Orellana <frederik@orellana.dk>
2022-03-17 11:24:50 +0100

    Only load js if logged in.

--- appinfo/app.php
+++ appinfo/app.php
@@ -2,7 +2,9 @@
 
 //\OCP\JSON::checkAppEnabled('user_orcid');
 
-\OCP\Util::addScript('files_zenodo', 'fileactions');
-\OCP\Util::addStyle('files_zenodo', 'style');
-
-\OCP\App::registerAdmin('files_zenodo', 'settings'); 
+if(\OC_User::isLoggedIn()){
+	\OCP\Util::addScript('files_zenodo', 'fileactions');
+	\OCP\Util::addStyle('files_zenodo', 'style');
+	
+	\OCP\App::registerAdmin('files_zenodo', 'settings'); 
+}
