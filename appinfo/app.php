e2ec46824cd71f3495fce8b6f82adac9e42df776
Frederik Orellana <frederik@orellana.dk>
2025-08-29 14:59:29 +0200

    Don't load scripts if not needed.

--- appinfo/app.php
+++ appinfo/app.php
@@ -2,7 +2,15 @@
 
 //\OCP\JSON::checkAppEnabled('user_orcid');
 
-if(\OC_User::isLoggedIn()){
+ if(\OC_User::isLoggedIn() &&!isset($_SERVER['REQUEST_URI']) ||
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/shared/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/public/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/files/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/remote.php/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/sharingin/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/sharingout/")!==0 &&
+ 		strpos($_SERVER['REQUEST_URI'], OC::$WEBROOT ."/groupfolders/")!==0
+ 		){
 	\OCP\Util::addScript('files_zenodo', 'fileactions');
 	\OCP\Util::addStyle('files_zenodo', 'style');
 	
